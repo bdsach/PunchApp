cca1ac839c9572da13104bd98e8db70a78be7358
Bandit Silachai <mbandit001@yahoo.co.th>
2023-11-22 17:30:05 +0700

    Initial Commit

---
+++ PunchApp/PunchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PunchAppApp.swift
+//  PunchApp
+//
+//  Created by Bandit Silachai on 22/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PunchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
