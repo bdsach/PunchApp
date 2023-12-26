028de00c14eb727808dc10cfa34aec0089400329
Bandit Silachai <mbandit001@yahoo.co.th>
2023-12-26 13:13:37 +0700

    🎉 Initial commit

--- PunchApp/ContentView.swift
+++ PunchApp/ContentView.swift
@@ -8,16 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animateFace = true
+    @State private var count: Int = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Punch Count: \(count)")
+                .font(.title).bold()
+                .animation(.bouncy, value: count)
+            Image("face")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .scaleEffect(animateFace ? 1 : 0.9)
+                .padding()
+                .onTapGesture {
+                    animateFace = false
+                    count += 1
+                    withAnimation(.spring(response: 0.4, dampingFraction: 0.8, blendDuration: 0)) {
+                        animateFace = true
+                    }
+                }
         }
         .padding()
     }
 }
+
 
 #Preview {
     ContentView()
